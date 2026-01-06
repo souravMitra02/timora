4c68bffb2878f52bacd10dd7b53325d9fa128883
souravMitra02 <inf.souravmitra@gmail.com>
2026-01-06 18:32:14 +0600

    connect mongodb database

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,1 +1,26 @@
-// Placeholder: MongoDB connect (no implementation)
+
+import { MongoClient, ServerApiVersion } from "mongodb";
+const uri = process.env.MONGODB_URI as string;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+ 
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+export default client;
+
