1b70f4c800f47fddef2d10289aa1a9cfdddaf661
souravMitra02 <inf.souravmitra@gmail.com>
2026-01-06 19:05:11 +0600

    add mongoose dependency and implement user model schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,1 +1,32 @@
-// Placeholder: User model (no implementation)
+import { Schema, model, models } from 'mongoose';
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim:true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    occupation: {
+        type: String,
+        enum: ["student", "professional"],
+        required: true,
+    },
+},
+    { timestamps: true });
+    const user = models.user || model('user', userSchema)
